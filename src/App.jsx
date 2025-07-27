0cf52ea8c16d9e4d5f4bdb93c0b5f87b62287025
Ravil2 <rafffa141105@gmail.com>
2025-07-27 16:48:04 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import Counter from './components/Counter'
-import ClassCounter from './components/ClassCounter'
+import { useState } from 'react'
+import Post from './components/Post'
 
 export default function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'JavaScript', body: 'JS - Programming language' },
+    { id: 2, title: 'JavaScript2', body: 'JS - Programming language' },
+    { id: 3, title: 'JavaScript3', body: 'JS - Programming language' },
+  ])
+
   return (
     <div>
-      <Counter />
-      <ClassCounter />
+      {posts.map((postItem) => (
+        <Post key={postItem.id} post={postItem} />
+      ))}
     </div>
   )
 }
---
+++ src/components/Post.jsx
@@ -0,0 +1,15 @@
+export default function Post({ post }) {
+  return (
+    <div className="post">
+      <div className="post__content">
+        <strong>
+          {post.id}. {post.title}
+        </strong>
+        <div>{post.body}</div>
+      </div>
+      <div className="post__btns">
+        <button>Удалить</button>
+      </div>
+    </div>
+  )
+}
