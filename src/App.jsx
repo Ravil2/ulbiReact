3c513713598321f64d25bd2b1057c812259a7bf6
Ravil2 <rafffa141105@gmail.com>
2025-07-31 13:44:53 +0500

    сортировка

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import PostList from './components/PostList'
 import PostForm from './components/PostForm'
+import MySelect from './components/UI/select/MySelect'
 
 export default function App() {
   const [posts, setPosts] = useState([
@@ -8,6 +9,7 @@ export default function App() {
     { id: 2, title: 'JavaScript2', body: 'JS - Programming language' },
     { id: 3, title: 'JavaScript3', body: 'JS - Programming language' },
   ])
+  const [selectedSort, setSelectedSort] = useState('')
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -17,10 +19,27 @@ export default function App() {
     setPosts(posts.filter((p) => p.id !== post.id))
   }
 
+  const sortPosts = (sort) => {
+    setSelectedSort(sort)
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+  }
+
   return (
     <div>
       <PostForm create={createPost} />
-      {posts.length !== 0 ? (
+      <hr style={{ margin: '15px 0px' }} />
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue="Сортировка по"
+          options={[
+            { value: 'title', name: 'По названию' },
+            { value: 'body', name: 'По описанию' },
+          ]}
+        />
+      </div>
+      {posts.length ? (
         <PostList posts={posts} title="Posts list 1" remove={removePost} />
       ) : (
         <h1 style={{ textAlign: 'center' }}>No posts!</h1>
