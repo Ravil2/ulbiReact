52b09fba794cb03520ccf0cc47318a25ee444e1c
Ravil2 <rafffa141105@gmail.com>
2025-08-05 16:27:15 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
 import PostList from './components/PostList'
 import PostForm from './components/PostForm'
-import { useMemo } from 'react'
 import PostFilter from './components/PostFilter'
 import MyModal from './components/UI/MyModal/MyModal'
 import MyButton from './components/UI/button/MyButton'
+import { usePost } from './hooks/usePosts'
 
 export default function App() {
   const [posts, setPosts] = useState([
@@ -14,22 +14,7 @@ export default function App() {
   ])
   const [filter, setFilter] = useState({ sort: '', query: '' })
   const [modal, setModal] = useState(false)
-
-  const sortedPosts = useMemo(() => {
-    console.log('ОТРАБОТАЛА ФУНКЦИЯ getSortedPosts')
-    if (filter.sort) {
-      return [...posts].sort((a, b) =>
-        a[filter.sort]?.localeCompare(b[filter.sort]),
-      )
-    }
-    return posts
-  }, [posts, filter.sort])
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter((post) =>
-      post.title.toLowerCase().includes(filter.query.toLowerCase()),
-    )
-  }, [filter.query, sortedPosts])
+  const sortedAndSearchedPosts = usePost(posts, filter.sort, filter.query)
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
