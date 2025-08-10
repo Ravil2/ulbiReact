bd11494d82499ddd11af9826cf17a33aa0a5a708
Ravil2 <rafffa141105@gmail.com>
2025-08-10 20:48:35 +0500

    exit

--- src/components/UI/Navbar/Navbar.jsx
+++ src/components/UI/Navbar/Navbar.jsx
@@ -1,12 +1,17 @@
 import { Link } from 'react-router'
+import MyButton from '../button/MyButton'
+import { useContext } from 'react'
+import { AuthContext } from '../../../context'
 
 export default function Navbar() {
+  const { isAuth, setIsAuth } = useContext(AuthContext)
   return (
     <div className="navbar">
       <div className="navbar__links">
         <Link to="/about">About</Link>
         <Link to="/posts">Posts</Link>
       </div>
+      <MyButton onClick={() => setIsAuth(false)}>Выйти</MyButton>
     </div>
   )
 }
